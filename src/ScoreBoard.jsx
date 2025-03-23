e3f821f1cbe445d53d7b4e51ca8e0d1d4c8ac2bb
Chel <chel.doyle@mscoding.org>
2025-03-22 22:06:32 -0500

    Score view updates
    Changed ScoreBoard.jsx to display all and tops based on buttons

--- src/ScoreBoard.jsx
+++ src/ScoreBoard.jsx
@@ -1,12 +1,53 @@
+import './App.css';
+import React, { useState } from 'react';
 
-import './App.css'
-import React from 'react';
+const scores = ['All', 'Top Five', 'Top Ten'];
 
-export default function ScoreBoard({leaderBoard})
-{
-  return(
+function DisplayScores({ leaderBoard, view }) {
+  let filteredLeaderBoard = leaderBoard;
+
+  if (view === 'Top Five') {
+    filteredLeaderBoard = leaderBoard.slice(0, 5);
+  } else if (view === 'Top Ten') {
+    filteredLeaderBoard = leaderBoard.slice(0, 10);
+  }
+
+  return (
+    <>
+      {filteredLeaderBoard.map((entry, index) => (
+        <tr key={index} className={index % 2 === 0 ? 'tableRowEven' : 'tableRowOdd'}>
+          <td>{index + 1}</td>
+          <td>{entry.username}</td>
+          <td>{entry.score}</td>
+        </tr>
+      ))}
+    </>
+  );
+}
+
+function SetScoreView({ activeView, setActiveView }) {
+  return (
+    <div>
+      {scores.map((score, index) => (
+        <button
+          key={index}
+          onClick={() => setActiveView(score)}
+          className={activeView === score ? 'active' : ''}
+        >
+          {score}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default function ScoreBoard({ leaderBoard }) {
+  const [activeView, setActiveView] = useState(scores[0]);
+
+  return (
     <>
       <h1 className='titleScore'>Leaderboard</h1>
+      <SetScoreView activeView={activeView} setActiveView={setActiveView} />
       <table className="read-the-docs">
         <tbody>
           <tr>
@@ -14,17 +55,9 @@ export default function ScoreBoard({leaderBoard})
             <th className='player'>Player</th>
             <th className='score'>Score</th>
           </tr>
-          {leaderBoard.map((entry, index) => (
-            <tr key={index} className={index % 2 === 0 ? 'tableRowEven' : 'tableRowOdd'}>
-              <td>{index + 1}</td>
-              <td>{entry.username}</td>
-              <td>{entry.score}</td>
-            </tr>
-          ))}
+          <DisplayScores leaderBoard={leaderBoard} view={activeView} />
         </tbody>
       </table>
     </>
-  )
+  );
 }
-
-  
